9f6c9985cfeee7aa5a9d31dead907327ef8ee91e
John Israel Aquino <jbaquino3@up.edu.ph>
2020-10-30 16:07:34 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <v-app>
+            <v-navigation-drawer app>
+                <!-- -->
+            </v-navigation-drawer>
+
+            <v-app-bar app color="#bbe6d6"  dense>
+                <!-- -->
+            </v-app-bar>
+
+            <!-- Sizes your content based upon application components -->
+            <v-main style="background-color:#c8ebdf">
+
+                <!-- Provides the application the proper gutter -->
+                <v-container fluid>
+
+                <!-- If using vue-router -->
+                <router-view></router-view>
+                </v-container>
+            </v-main>
+
+            <v-footer app>
+                <!-- -->
+            </v-footer>
+        </v-app>
+    </div>
 </template>
